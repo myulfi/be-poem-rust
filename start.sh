85a8a4aad8a5698621e0eeec86719d968a8801b7
Mulyadi Yulfi <myulfi@xl.co.id>
2025-09-16 14:04:51 +0700

    Update doc

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-nohup ./home/ubuntu/prod/be-poem-rust > ./home/ubuntu/prod/ext/log/main.log 2>&1 &
+cd /home/ubuntu/production
+nohup /home/ubuntu/production/be-poem-rust > /home/ubuntu/production/ext/log/main.log 2>&1 &
 echo "start be-poem-rust done"
